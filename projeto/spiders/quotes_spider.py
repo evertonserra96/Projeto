c9e37f3205a3f76708da1c649b19d460b6d3e163
evertonserra96 <101928797+evertonserra96@users.noreply.github.com>
2022-03-21 16:53:02 -0300

    Update quotes_spider.py
    filtro quotes

--- projeto/spiders/quotes_spider.py
+++ projeto/spiders/quotes_spider.py
@@ -1,17 +1,27 @@
 import scrapy
 
 
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+class ToScrapeSpiderXPath(scrapy.Spider):
+    name = 'quotes'
     start_urls = [
-        'https://quotes.toscrape.com/page/1/',
-        'https://quotes.toscrape.com/page/2/',
+        'http://quotes.toscrape.com/',
     ]
 
     def parse(self, response):
-        for quote in response.css('div.quote'):
+        for quote in response.xpath('//div[@class="quote"]'):
             yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('small.author::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall(),
+                'text': quote.xpath('./span[@class="text"]/text()').extract(),
+                'author': quote.xpath('.//small[@class="author"]/text()').extract(),
+                'tags': quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').extract()
             }
+
+        next_page_url = response.xpath('//li[@class="next"]/a/@href').extract_first()
+        if next_page_url is not None:
+            yield scrapy.Request(response.urljoin(next_page_url))
+
+
+#scrapy shell
+#response.xpath("//span[@class='text']/text()").extracta()
+#response.css("a").xpath("@href").extract()
+#response.xpath('//div//span//a[re:test(@href,"Mark-Twain")]')
+#response.css("li.next a").xpath("@href").extract()
